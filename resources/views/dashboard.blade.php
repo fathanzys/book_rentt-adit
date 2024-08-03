02f6dcc87b05beab210136fc7b5bcb4b41f72491
fathanzys <fathanamanda12@gmail.com>
2024-08-03 14:48:03 +0700

    te

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.mainlayouts')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat laborum inventore quaerat id nihil atque consequuntur quasi nisi officia dolor aut qui quo accusamus similique in dignissimos, nesciunt, beatae magnam.
+    </p>
+@endsection
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.mainlayouts')
+
+@section('title', 'Profile')
+
+@section('content')
+@endsection
